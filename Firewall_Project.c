a0d76fe97426beb374ee527717d924df76b9732d
Lucas Turossi <lucasturossi@protonmail.com>
2023-08-14 23:22:32 -0300

    Add files via upload

--- Firewall_Project.c
+++ Firewall_Project.c
@@ -1,12 +1,33 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
 
 void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, const unsigned char *packet) {
-    printf("Captured a packet of lenght %d\n", pkthdr -> len);
+    printf("Captured a packet of length %d\n", pkthdr->len);
+
+const unsigned char *ethernet_header = packet;
+    int ether_type = ntohs((ethernet_header[12] << 8) | ethernet_header[13]);
+
+
+if (ether_type == 0x0800){
+    const unsigned char *ip_header = packet + 14;
+    int ip_version = (ip_header[0] >> 4) & 0xF;
+    if (ip_version == 4 ){
+        int ip_header_length = (ip_header[0] & 0xF) *4;
+        struct in_addr src_ip, dst_ip;
+        memcpy(&src_ip, ip_header + 12, sizeof(struct in_addr));
+        memcpy(&dst_ip, ip_header + 16, sizeof(struct in_addr));
+
+        printf("Source IP: %s\n", inet_ntoa(src_ip));
+        printf("Destination IP: %s\n", inet_ntoa(dst_ip));
+        }       
+    }
 }
 
-int main(void) {
+
+
+int main(void){
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
 
